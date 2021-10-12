85ad765215a6f5e447c5d6b0222d4c16444d8378
ilhamhanifpamulia <john.ilham58@gmail.com>
2021-10-13 00:12:52 +0700

    Problem 10

---
+++ Assets/Script/Control.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+     // gerakkanan kiri
+    private Rigidbody2D rb;
+    public float kecepatan;
+    public Vector2 gerakan;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // gerak kanan kiri
+        gerakan.x = Input.GetAxisRaw("Horizontal");
+        gerakan.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+    // gerak kanan kiri
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + gerakan * kecepatan * Time.fixedDeltaTime);
+    }
+}
+
